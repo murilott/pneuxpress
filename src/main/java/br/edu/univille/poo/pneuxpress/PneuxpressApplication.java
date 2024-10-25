0f598ea500467e33370d200c6337627054d3ccd5
murilott <murilo_teichert@hotmail.com>
2024-10-24 21:39:03 -0300

    controller, repository, entidade, service e db

---
+++ src/main/java/br/edu/univille/poo/pneuxpress/PneuxpressApplication.java
@@ -0,0 +1,13 @@
+package br.edu.univille.poo.pneuxpress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PneuxpressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PneuxpressApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/univille/poo/pneuxpress/PneuxpressApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.univille.poo.pneuxpress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PneuxpressApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
