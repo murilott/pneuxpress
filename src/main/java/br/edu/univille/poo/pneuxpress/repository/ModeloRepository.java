6cb40b4bc6758e903bbcfab984047efc9f7e1981
murilott <murilo_teichert@hotmail.com>
2024-11-07 21:20:41 -0300

    Remoção da entidade Modelo

--- src/main/java/br/edu/univille/poo/pneuxpress/repository/ModeloRepository.java
+++
@@ -1,9 +0,0 @@
-package br.edu.univille.poo.pneuxpress.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.edu.univille.poo.pneuxpress.entity.Modelo;
-
-public interface ModeloRepository extends JpaRepository<Modelo, Long>{
-    
-}
